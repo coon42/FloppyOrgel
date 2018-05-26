d1e2da166892c745123cb4ca27bcbbed44fe15d3
coon <coon@mailbox.org>
2018-05-26 03:11:16 +0200

    add FlyFi submodule + arduino firmware

---
+++ Arduino/FloppyM0dule_conf.h
@@ -0,0 +1,15 @@
+// Pin mappings
+#define PIN_DRIVE_SELECT A0
+#define PIN_DIRECTION    A1
+#define PIN_STEP         10 // PB2
+
+#define PIN_RS485_DE     8 // PB0
+#define PIN_RS485_NRE    9 // PB1
+
+#define PIN_SHIFT_REGISTER_RCLK A2
+#define PIN_UNDER_CHIP_LED A3
+
+// Floppy settings
+#define MAX_STEP_POSITION 79 // A floppy disk has 80 tracks (0 - 79). 
+#define MAX_FREQUENCY 393 // TODO: Define per floppy drive and save into atmels flash chip?
+
